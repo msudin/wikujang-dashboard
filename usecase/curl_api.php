0e42726099be9bf2498e1fc48e1f00ae1643395a
Muhammad Sholehuddin <udin0141@gmail.com>
2022-09-11 00:47:36 +0700

    adjust curl timeout

--- usecase/curl_api.php
+++ usecase/curl_api.php
@@ -21,7 +21,9 @@ function callAPI($method, $url, $data){
 
    // OPTIONS:
    curl_setopt($curl, CURLOPT_URL, $url);
-   curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+   curl_setopt($ch, CURLOPT_TIMEOUT, 0);
+   curl_setopt($ch, CURLOPT_TIMEOUT, 400);
+   curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
    // EXECUTE:
    $result = curl_exec($curl);
